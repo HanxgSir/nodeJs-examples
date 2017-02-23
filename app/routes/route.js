fbd107841a5bb4fc9925fc67947293bb4cf12163
HanxgSir <18304018283@163.com>
2017-02-23 17:21:50 +0800

    fix 提交一下 暂时有bug

--- app/routes/route.js
+++ app/routes/route.js
@@ -1,12 +1,26 @@
 /**
  * Created by Administrator on 2017/2/23.
  */
+const mongoose = require('mongoose');
+const db = mongoose.connect('mongodb://localhost/test'); //连接到一个test的数据库
+db.connection.on('error', function(error) {
+    console.log(error);
+});
+db.connection.on("open", function () {
+    console.log("——数据库连接成功！——");
+});
 module.exports = function (app) {
     // 处理第一次打开时的请求
-    app.get('/', function (req, res) {
-        console.log('server is start ');
-        res.render('login/login')
-    });
+    //app.get('/', function (req, res) {
+    //    console.log('server is start ');
+    //    res.render('login/login')
+    //});
+
+    // 登录页路由
+    app.route('/')
+        .get(function (req, res) {
+            res.render('login/login')
+        });
 
     // 登录
     app.post('/login', function (req, res) {
@@ -16,8 +30,25 @@ module.exports = function (app) {
         res.send({status: 0, msg: '登录成功'});
     });
 
+    // 注册页路由
     app.route('/register')
         .get(function (req, res) {
             res.render('register/register');
         });
+
+    // 注册
+    app.post('/register', function (req, res) {
+        console.log('register click');
+        console.log(req.body);
+        var UserSchema = new mongoose.Schema({
+            email:{type:String},
+            password:{type:Number},
+            nickname:{type:String},
+            phone:{type:String}
+        });
+        var UserModel = db.model('User',UserSchema);
+        var UserEntity = new UserModel({name:'User'});
+        UserEntity.save();
+        res.send({status:0})
+    })
 };
